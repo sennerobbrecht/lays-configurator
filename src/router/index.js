9d1fa6910c68facbd757c8a2c9e9d2adf362affa
SenneRobbrecht <senne.robbrecht@dodentocht.be>
2025-11-18 23:34:51 +0100

    feat: routes aanmaken

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import CreateBagPage from '../views/CreateBagPage.vue'
+import SuccessPage from '../views/SuccessPage.vue'
+import DashboardPage from '../views/DashboardPage.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'CreateBag',
+      component: CreateBagPage
+    },
+    {
+      path: '/success',
+      name: 'Success',
+      component: SuccessPage
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: DashboardPage
+    }
+  ],
 })
 
 export default router
