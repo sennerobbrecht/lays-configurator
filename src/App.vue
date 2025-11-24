d8c3e5a98cff55352cf637d9b0cc886dbaa3dd58
SenneRobbrecht <senne.robbrecht@dodentocht.be>
2025-11-24 11:25:19 +0100

    feat: cold start backend api

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,18 @@
 </template>
 
 <script setup>
+import axios from "axios";
+import { onMounted } from "vue";
+
++
+onMounted(() => {
+  axios
+    .get("https://lays-api-1.onrender.com/api/v1/user/ping", {
+      timeout: 60000,
+    })
+    .then(() => console.log("Backend warmed up ✔️"))
+    .catch(() => console.log("Backend waking up... ⏳"));
+});
 </script>
 
 <style>
