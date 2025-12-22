8a981d13356590257f603d43688a54b5e21f02eb
SenneRobbrecht <senne.robbrecht@dodentocht.be>
2025-12-22 20:42:37 +0100

    feat: security on the pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import DashboardPage from '../views/DashboardPage.vue'
 import CreateBagPage from '../views/CreateBagPage.vue'
 import SuccessPage from '../views/SuccessPage.vue'
@@ -6,7 +7,6 @@ import LoginPage from '../views/LoginPage.vue'
 import RegisterPage from '../views/RegisterPage.vue'
 import VotesPage from '../views/VotesPage.vue'
 import UpdateBagPage from '../views/UpdateBagPage.vue'
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,33 +21,49 @@ const router = createRouter({
       name: 'Register',
       component: RegisterPage
     },
+
     {
       path: '/dashboard',
       name: 'Dashboard',
-      component: DashboardPage
+      component: DashboardPage,
+      meta: { requiresAuth: true }
     },
     {
       path: '/create',
       name: 'CreateBag',
-      component: CreateBagPage
+      component: CreateBagPage,
+      meta: { requiresAuth: true }
     },
     {
       path: '/success',
       name: 'Success',
-      component: SuccessPage
+      component: SuccessPage,
+      meta: { requiresAuth: true }
     },
     {
       path: '/vote',
       name: 'Votes',
-      component: VotesPage
+      component: VotesPage,
+      meta: { requiresAuth: true }
     },
     {
-  path: "/update/:id",
-  name: "UpdateBag",
-  component: UpdateBagPage
-}
+      path: '/update/:id',
+      name: 'UpdateBag',
+      component: UpdateBagPage,
+      meta: { requiresAuth: true }
+    }
+  ]
+})
 
-  ],
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+
+  if (to.meta.requiresAuth && !token) {
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
